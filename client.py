68bc49f2570c93fc2154cabc98678a6562a9faf7
wwwwwdy <ant2.8949@gmail.com>
2022-07-29 11:53:25 +0300

    added client & validator

---
+++ client.py
@@ -0,0 +1,30 @@
+from typing import ClassVar
+
+
+from aiohttp import TCPConnector
+
+from schema import Charge
+from validator import SchemaValidator
+from abstract_client import AbstractInteractionClient
+
+
+class CloudPaymentClient(AbstractInteractionClient):
+    BASE_URL: ClassVar[str] = "https://api.cloudpayments.ru"
+    
+    def __init__(self, base_url=None, timeout=None, loop=None) -> None:
+        self.BASE_URL = base_url if not base_url else self.BASE_URL
+        self.CONNECTOR = TCPConnector()
+        self.SERVICE = "service"
+        self.REQUEST_TIMEOUT = timeout
+        self._loop = loop
+        super().__init__()
+
+    @SchemaValidator(Charge)
+    async def charge(self, data=None):
+        response = await self._request(
+            "POST",
+            self.endpoint_url("payments/charge"),
+            data=data,
+        )
+
+        return response
---
+++ validator.py
@@ -0,0 +1,26 @@
+from functools import wraps
+
+import marshmallow_dataclass
+
+
+class SchemaValidator:
+    def __init__(self, schema):
+        self.schema = schema
+
+    def __call__(self, func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            if not (data := kwargs.get("data")):
+                _, data = args
+
+            schema = marshmallow_dataclass.class_schema(self.schema)()
+            schema_object = schema.load(data)
+            schema_dict = {
+                k: v for k, v 
+                in schema.dump(schema_object).items()
+                if v is not None
+            }
+            result = func(args[0], schema_dict)
+            return result
+
+        return wrapper
